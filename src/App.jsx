08e0ac5bbd2052604be095df7fb33541ba4de75b
Patryk <wafeldzn@gmail.com>
2024-10-31 12:04:30 +0100

    Added PC site

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import About from "./Components/About/About"
+import Campus from "./Components/Campus/Campus"
+import Contact from "./Components/Contact/Contact"
+import Footer from "./Components/Footer/Footer"
+import Hero from "./Components/Hero/Hero"
+import Navbar from "./Components/Navbar/Navbar"
+import Programs from "./Components/Programs/Programs"
+import Testimonials from "./Components/Testimonials/Testimonials"
+import Title from "./Components/Title/Title"
+
+
+const App = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Hero/>
+        <div className="container">
+          <Title subTitle='Our PROGRAM' title='What We Offer'/>
+          <Programs/>
+          <About/>
+          <Title subTitle='Gallery' title='Campus Photos'/>
+          <Campus/>
+          <Title subTitle='TESTIMONIALS' title='What Student Says'/>
+          <Testimonials/>
+          <Title subTitle='Contact Us' title='Get in touch'/>
+          <Contact/>
+          <Footer/>
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,84 @@
+import './Navbar.css'
+import { useState, useEffect } from 'react'
+import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll'
+
+const Navbar = () => {
+	const [sticky, setSticky] = useState(false)
+
+	useEffect(() => {
+		window.addEventListener('scroll', () => {
+			window.scrollY > 50 ? setSticky(true) : setSticky(false)
+		})
+	}, [])
+
+	return (
+		<nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+			<img
+				src={logo}
+				alt='Edusity logo'
+				className='logo'
+			/>
+			<ul>
+				<li>
+					<Link
+						to='hero'
+						smooth={true}
+						offset={0}
+						duration={500}>
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link
+						to='program'
+						smooth={true}
+						offset={-260}
+						duration={500}>
+						Program
+					</Link>
+				</li>
+				<li>
+					<Link
+						to='about'
+						smooth={true}
+						offset={-150}
+						duration={500}>
+						About us
+					</Link>
+				</li>
+				<li>
+					<Link
+						to='campus'
+						smooth={true}
+						offset={-260}
+						duration={500}>
+						Campus
+					</Link>
+				</li>
+				<li>
+					<Link
+						to='testimonials'
+						smooth={true}
+						offset={-260}
+						duration={500}>
+						Testimonials
+					</Link>
+				</li>
+				<li>
+					<button className='btn'>
+						<Link
+							to='contact'
+							smooth={true}
+							offset={-260}
+							duration={500}>
+							Contact us
+						</Link>
+					</button>
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navbar
